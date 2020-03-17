518356654632a3ed2d87e7bfce162141b505881f
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-17 12:55:56 -0700

    store and actions ready

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import rootReducer from './Reducers/rootReducer';
+
+// Initialize Redux Store
+const store = createStore(rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<Provider store={store}>
+  <App />
+</Provider>
+, rootElement);
