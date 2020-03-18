797c6db96b199e66708bacbf73d3701d0989a799
Rudy Goldhaber <rgoldhaber24@gmail.com>
2020-03-17 21:30:58 -0700

    Fully working using Hooks

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
 import React from 'react';
-import { connect } from 'react-redux'
+import { shallowEqual, useSelector } from 'react-redux'
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
-const App = (props) => {
-  console.log(props)
-  console.log(`buying: `, props.car.features)
+const App = () => {
+  const { car, additionalPrice, additionalFeatures } = useSelector( state => ({
+    car: state.car,
+    additionalPrice: state.additionalPrice,
+    additionalFeatures: state.additionalFeatures
+  }), shallowEqual)
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={props.car} />
-        <AddedFeatures car={props.car} />
+        <Header car={car} />
+        <AddedFeatures car={car} />
       </div>
       <div className="box">
-        <AdditionalFeatures additionalFeatures={props.additionalFeatures} />
-        <Total car={props.car} additionalPrice={props.additionalPrice} />
+        <AdditionalFeatures additionalFeatures={additionalFeatures} />
+        <Total car={car} additionalPrice={additionalPrice} />
       </div>
-      {/* <button onClick={() => console.log(props)}>props log</button> */}
     </div>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    car: state.car,
-    additionalPrice: state.additionalPrice,
-    additionalFeatures: state.additionalFeatures
-  }
-}
-
-export default connect(mapStateToProps, null)(App);
+export default App;
